214e2edac2a1d32b1ecbefc876eee55517ffecec
Harsh Banjare <harsh.12110689@lpu.in>
2023-08-28 21:26:22 +0530

    Bug Fix

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
 document.addEventListener("scroll", () => {
   document.querySelector(".bg-roll").play();
 });
