9acadefa579f35dc1164edafe75fcd0efa2133fa
Harsh Banjare <harsh.12110689@lpu.in>
2023-08-29 00:30:36 +0530

    Bug Fix

--- nav.js
+++ nav.js
@@ -4,3 +4,23 @@ const toggle_nav = () => {
 document.querySelector(".mobile-toggle").addEventListener("click", toggle_nav);
 
 document.querySelector(".close-mobile").addEventListener("click", toggle_nav);
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("nav");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    var $mobile_toggle = $(".mobile-toggle > span");
+    $mobile_toggle.toggleClass(
+      "scrolled",
+      $(this).scrollTop() > $mobile_toggle.height()
+    );
+  });
+});
+
+const scroll_to = (section) => {
+  console.log(section);
+  document.querySelector(section).scrollIntoView({
+    behavior: "smooth",
+  });
+  toggle_nav();
+};
